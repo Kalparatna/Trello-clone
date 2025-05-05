dfe60e9a10db338baca5ae68c4bbd1cfcca351f7
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-05-05 11:47:06 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -3,4 +3,4 @@ const app = require('../server');  // Import the Express app
 const serverless = require('serverless-http');  // Import serverless-http
 
 // Export the handler for serverless functions
-module.exports.handler = serverless(app);
+module.exports.handler = serverless(app); 
