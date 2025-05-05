5936f3ae6784db8108eb8af0ae4c8e8934ce91a0
Kalparatna Mahajan <125982626+Kalparatna@users.noreply.github.com>
2025-05-05 12:01:13 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,5 @@
 // api/index.js
-const app = require('../server');  // Import the Express app
+const app = require('./server');  // Import the Express app
 const serverless = require('serverless-http');  // Import serverless-http
 
 module.exports.handler = serverless(app);  // Wrap the app with serverless-http for Vercel
